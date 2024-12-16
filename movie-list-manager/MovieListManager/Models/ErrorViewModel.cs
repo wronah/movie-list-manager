1660e24b65ceb94be61a0607ba5ac7459ac3f617
wronah <hubertwro7@gmail.com>
2024-12-16 23:59:00 +0100

    create project

---
+++ movie-list-manager/MovieListManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieListManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
