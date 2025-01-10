0d5f5859197999d70a34127947670d7e5e2f3b1a
wronah <hubertwro7@gmail.com>
2025-01-10 23:36:27 +0100

    add register login and logout

---
+++ movie-list-manager/MovieListManager/Areas/Identity/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieListManager.Areas.Identity.Data;
+
+namespace MovieListManager.Areas.Identity.Configurations
+{
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.Property(x => x.FirstName).HasMaxLength(255);
+            builder.Property(x => x.LastName).HasMaxLength(255);
+        }
+    }
+}
--- movie-list-manager/MovieListManager/Areas/Identity/Data/ApplicationUser.cs
+++ movie-list-manager/MovieListManager/Areas/Identity/Data/ApplicationUser.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace MovieListManager.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
 }
 
